eef202aa8090dc0a7563a8a6a6f214dcd0237cbf
awaters9@masonlive.gmu.edu <awaters9@masonlive.gmu.edu>
2020-02-07 17:09:30 -0500

    Added framework scripts, removed bloated stuff

---
+++ Assets/Scripts/FrameworkScripts/StateMachine.cs
@@ -0,0 +1,45 @@
+﻿namespace StateMachine
+{
+    public class StateMachine<T>
+    {
+        public State<T> currentState { get; private set; }
+        public T Owner;
+
+        public StateMachine(T o)
+        {
+            Owner = o;
+            currentState = null;
+        }
+
+        public void ChangeState(State<T> newState)
+        {
+            if(currentState != null)
+                currentState.ExitState(Owner);
+            currentState = newState;
+            currentState.EnterState(Owner);
+        }
+
+        public void Update()
+        {
+            if (currentState != null)
+            {
+                currentState.UpdateState(Owner);
+            }
+        }
+
+        //stops the current state
+        public void HaltState()
+        {
+            if (currentState != null)
+                currentState.ExitState(Owner);
+            currentState = null;
+        }
+    }
+
+    public abstract class State<T>
+    {
+        public abstract void EnterState(T owner);
+        public abstract void ExitState(T owner);
+        public abstract void UpdateState(T owner);
+    }
+}
---
+++ Assets/Scripts/FrameworkScripts/VarFloat.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class VarFloat : ScriptableObject
+{
+    public float value;
+}

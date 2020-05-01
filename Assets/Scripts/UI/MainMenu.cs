960a967f821a92176058013a9294dd9b44644131
Tyler Smith <commanderblondie@gmail.com>
2020-05-01 02:01:59 -0400

    Main menu and game over panels in progress.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameOverUI_Test");
+    }
+
+    public void OpenSettings()
+    {
+        //Open Settings
+    }
+
+    public void OpenHowToPlay()
+    {
+        //Open How to play
+    }
+
+    public void QuitToDesktop()
+    {
+        Application.Quit();
+    }
+}

f180d9e2328346898166f38339216dbc469a528b
Derrick Lemmon <dsl1130@gmail.com>
2020-05-04 13:44:26 -0400

    Walls have colliders and both those and the floor are a part of the Static Environment layer fixing a lot of wall related issues. Also added an invisible plane that follows the camera in order to fix aiming bug.

--- Assets/Scripts/Scenes/ChooseWall.cs
+++ Assets/Scripts/Scenes/ChooseWall.cs
@@ -15,6 +15,11 @@ public class ChooseWall : MonoBehaviour
 
         int index = (int)UnityEngine.Random.Range(0, walls.items.Count);
         GameObject wall = GameObject.Instantiate((GameObject)walls.items.ToArray().GetValue(index));
+        wall.layer = LayerMask.NameToLayer("StaticEnvironment");
+        wall.AddComponent<MeshCollider>();
+
+        wall.AddComponent<BoxCollider>();
+        wall.GetComponent<BoxCollider>().size = new Vector3(wall.GetComponent<BoxCollider>().size.x, wall.GetComponent<BoxCollider>().size.y, 0.5f);
         wall.transform.parent = gameObject.transform;
         wall.transform.localPosition = new Vector3(0f, 0f, 0f);
         wall.transform.localRotation = Quaternion.Euler(0, 0, 0);
